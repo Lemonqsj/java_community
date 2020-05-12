457a1f13e122acd010a590a03cafb539b9235fb4
lemon <lemonqsj@163.com>
2020-05-13 00:23:03 +0800

    增加token验证持久化登录

--- src/main/java/com/lemon/spring_first/controller/AuthorizeController.java
+++ src/main/java/com/lemon/spring_first/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.lemon.spring_first.controller;
 
 import com.lemon.spring_first.dto.AccessTokenDto;
 import com.lemon.spring_first.dto.GithubUser;
+import com.lemon.spring_first.mapper.UserMapper;
+import com.lemon.spring_first.model.User;
 import com.lemon.spring_first.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,7 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -27,10 +32,15 @@ public class AuthorizeController {
     @Value("${github.rediect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
+
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -41,7 +51,16 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getGithubUser(accessToken);
         if (githubUser!=null){
             //登录成功 将用户信息写入session中
-            request.getSession().setAttribute("user",githubUser);
+            User user=new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insertUser(user);
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else {
             //登录失败 返回首页
--- src/main/java/com/lemon/spring_first/controller/IndexController.java
+++ src/main/java/com/lemon/spring_first/controller/IndexController.java
@@ -1,13 +1,38 @@
 package com.lemon.spring_first.controller;
 
+import com.lemon.spring_first.mapper.UserMapper;
+import com.lemon.spring_first.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    UserMapper userMapper;
     @GetMapping("/")
-    public String test() {
+    public String test(HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user= userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+
+            }
+        }
+
+
+
         return "index";
     }
 }
